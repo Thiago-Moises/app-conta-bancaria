1b49ae189b1068df95d78cf9f19139e790609f79
Thiago Moisés Braga Montalvão <111362865+Thiago-Moises@users.noreply.github.com>
2024-03-19 14:54:53 -0300

    Update ContaTerminal.java
    Desafio comitado.

--- app-conta-bancaria/conta-bancaria-execucao/src/ContaTerminal.java
+++ app-conta-bancaria/conta-bancaria-execucao/src/ContaTerminal.java
@@ -1,6 +1,24 @@
-public class ContaTerminal {
+import java.util.Scanner;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
